332f9c729778ef26b5abc5a2ed83e9f1a946c6a1
Mika <46321023+hoffrenm@users.noreply.github.com>
2023-02-27 12:11:50 +0200

    Initial commit

---
+++ src/main/java/com/example/easpringapi/EaSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.easpringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaSpringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/easpringapi/EaSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easpringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
