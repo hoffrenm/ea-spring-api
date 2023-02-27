a8f70811fcb90aa1f12fa394cf7685ac0b375ebb
Mika <46321023+hoffrenm@users.noreply.github.com>
2023-02-27 13:19:26 +0200

    Add annotations

--- src/main/java/com/example/easpringapi/models/Character.java
+++ src/main/java/com/example/easpringapi/models/Character.java
@@ -1,4 +1,12 @@
 package com.example.easpringapi.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+@Entity
+@Data
 public class Character {
+    
+    @Id
+    private Long id;
 }
--- src/main/java/com/example/easpringapi/repositories/CharacterRepository.java
+++ src/main/java/com/example/easpringapi/repositories/CharacterRepository.java
@@ -1,4 +1,9 @@
 package com.example.easpringapi.repositories;
 
-public class CharacterRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Integer> {
+
 }
